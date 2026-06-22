58de6e17a390a6fc0cf1f75ee48d90222b73e11b
roottp <mezbah488-ops>
2026-06-22 14:45:48 +0600

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1101,7 +1101,7 @@ require('lazy').setup({
   require 'kickstart.plugins.nabla',
   require 'kickstart.plugins.twilight',
   require 'kickstart.plugins.zen-mode',
-  require 'kickstart.plugins.treesitter',
+  --require 'kickstart.plugins.treesitter',
   --require 'kickstart.plugins.true-zen',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
